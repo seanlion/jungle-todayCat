b131e163621d54bdb647e32f053f786c9bbd547e
seanhuh <skyhs1100@gmail.com>
2020-12-09 19:09:04 +0900

    login complete

--- main/signup.py
+++ main/signup.py
@@ -1,32 +1,19 @@
 from main import *
 
-@app.route('/signup')
-def registerPage():
-    return render_template('signup.html')
 
-@app.route('/signup', methods=["POST"])
+@app.route('/signup', methods=["GET","POST"])
 def register():
-    userList = mongo.db.users.find({}, {"_id":False})
-    email_receive = request.form["emailText"]
-    password1_receive = request.form["password1"]
-    password2_receive = request.form["password2"]
-    name_receive = request.form["nameTextarea"]
-    # 받아온 정보가 빈칸이면 에러 메시지 반환
-    if email_receive or password1_receive or password2_receive or name_receive is None:
-        flash("입력이 완료되지 않았습니다.")
-        return redirect(url_for("registerPage"))
-    # email 중복 검사
-    userEmails = userList["email"]
-    if email_receive in userEmails:
-        flash("이미 가입되어 있는 이메일입니다.", "error")
-        return redirect(url_for("registerPage"))
-    # password 일치 검사
-    elif password1_receive != password2_receive:
-        flash("비밀번호가 일치하지 않습니다.", "error")
-        return redirect(url_for("registerPage"))
-    else:
+    if request.method == "POST":
+        userList = mongo.db.users.find({}, {"_id":False})
+        email_receive = request.form["emailText"]
+        password1_receive = request.form["password1"]
+        password2_receive = request.form["password2"]
+        name_receive = request.form["nameTextarea"]
+
         password_hash = bcrypt.generate_password_hash(password1_receive)
         user = {"email": email_receive, "password": password_hash, "name": name_receive}
         mongo.db.users.insert_one(user)
-        flash("회원가입이 완료되었습니다.")
-        return redirect(url_for("loginPage"))
+        flash({"msg":"회원가입이 완료되었습니다."})
+        return redirect(url_for("loginpage"))
+    else :
+        return render_template('signup.html')
